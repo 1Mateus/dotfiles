c9f422a0a2d01e6e4e40fb053b73c9bc60eff07e
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-12-10 13:38:56 -0300

    feat: updated cask.sh and install.sh

--- cask.sh
+++ cask.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env bash
 
 # Install brew cask and apps
-brew install authy
+# brew install authy
 brew install alfred
 brew install iterm2
-brew install caffeine
-# brew install cakebrew
-brew install dropbox
-brew install discord
+#brew install caffeine
+#brew install cakebrew
+#brew install dropbox
+#brew install discord
 brew install github
 brew install google-chrome
 brew install imageoptim
 #brew install java
-# brew install postman
-brew install qlmarkdown # Add quicklook view for markdown files
-brew install spectacle # Window shortcuts
-brew install vlc
-brew install the-unarchiver
-brew install sublime-text
+#brew install postman
+#brew install qlmarkdown # Add quicklook view for markdown files
+#brew install spectacle # Window shortcuts
+#brew install vlc
+#brew install the-unarchiver
+#brew install sublime-text
 
 # Install privacy utils
-brew install cryptomator # Encrypt iCloud/Dropbox folders
-brew install knockknock # See what's persistently installed on your Mac.
-brew install blockblock # Block/notify launch agents
-brew install security-growler # Notify security events
-# brew install lulu # Block outgoing connections, little-snitch alternative
-brew install do-not-disturb # https://objective-see.org/products/dnd.html
-brew install taskexplorer
-brew install onyx # https://titanium-software.fr/en/onyx.html
-brew install keepassxc # https://keepassxc.org
+#brew install cryptomator # Encrypt iCloud/Dropbox folders
+#brew install knockknock # See what's persistently installed on your Mac.
+#brew install blockblock # Block/notify launch agents
+#brew install security-growler # Notify security events
+#brew install lulu # Block outgoing connections, little-snitch alternative
+#brew install do-not-disturb # https://objective-see.org/products/dnd.html
+#brew install taskexplorer
+#brew install onyx # https://titanium-software.fr/en/onyx.html
+#brew install keepassxc # https://keepassxc.org
 #brew install flux
 #brew install flycut
 #brew install steam
@@ -42,15 +42,15 @@ brew install keepassxc # https://keepassxc.org
 #brew install tribler # Download and watch videos
 
 # Install apps from App Store
-mas lucky Things3
-# mas lucky Bear
-# mas lucky Wallcat
-mas lucky Dato
-mas lucky Pandan
-mas lucky Whatsapp
-# mas lucky Slack
-# mas lucky Banktivity
+#mas lucky Things3
+#mas lucky Bear
+#mas lucky Wallcat
+#mas lucky Dato
+#mas lucky Pandan
+#mas lucky Whatsapp
+mas lucky Slack
+#mas lucky Banktivity
 
 # Add sublime command
-ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
-ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+#ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+#ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
--- install.sh
+++ install.sh
@@ -19,15 +19,15 @@ brew upgrade
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+# brew install coreutils
+# ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
-brew install moreutils
+# brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
+# brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed
+# brew install gnu-sed
 
 # Install zsh
 brew install zsh
@@ -46,10 +46,10 @@ git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/pl
 brew install wget
 
 # Install more recent versions of some macOS tools.
-brew install vim
+# brew install vim
 brew install grep
 brew install openssh
-brew install screen
+# brew install screen
 
 # Install R and stuff
 # brew install r
@@ -62,89 +62,89 @@ brew install sfnt2woff-zopfli
 brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/sbin/airport
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hashcat
-brew install hashcat-utils
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install ngrep
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+# sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/sbin/airport
+# brew install aircrack-ng
+# brew install bfg
+# brew install binutils
+# brew install binwalk
+# brew install cifer
+# brew install dex2jar
+# brew install dns2tcp
+# brew install fcrackzip
+# brew install foremost
+# brew install hashpump
+# brew install hashcat
+# brew install hashcat-utils
+# brew install hydra
+# brew install john
+# brew install knock
+# brew install netpbm
+# brew install nmap
+# brew install ngrep
+# brew install pngcheck
+# brew install socat
+# brew install sqlmap
+# brew install tcpflow
+# brew install tcpreplay
+# brew install tcptrace
+# brew install ucspi-tcp # `tcpserver` etc.
+# brew install xpdf
+# brew install xz
 
 # Install other useful binaries.
-brew install ack
-brew install dark-mode
+# brew install ack
+# brew install dark-mode
 brew install git
 brew install git-extras
 brew install imagemagick --with-webp
-brew install p7zip
-brew install speedtest-cli
+# brew install p7zip
+# brew install speedtest-cli
 brew install ssh-copy-id
-brew install pandoc # convert doc files
-brew install bat # better cat
+# brew install pandoc # convert doc files
+# brew install bat # better cat
 brew install gh
 brew install git-town # git workflows
-brew install htop
-brew install mackup # app backups to cloud
-brew install innotop # innodb top cmd
-# brew install mycli #mysql client
-brew install mas
-brew install pv # pipeviewer
-brew install watch # watcher
-brew install jq # json bash processor
-brew install neofetch # Awesome system info
-# brew install utm # VM for M1
-# brew install fastlane #build ios apps
-# brew install cocoapods #ios dependencies
-# brew tap AlexanderWillner/tap
-# brew install things.sh
-brew install just # Just command runner
+#brew install htop
+#brew install mackup # app backups to cloud
+#brew install innotop # innodb top cmd
+#brew install mycli #mysql client
+#brew install mas
+#brew install pv # pipeviewer
+#brew install watch # watcher
+#brew install jq # json bash processor
+#brew install neofetch # Awesome system info
+#brew install utm # VM for M1
+#brew install fastlane #build ios apps
+#brew install cocoapods #ios dependencies
+#brew tap AlexanderWillner/tap
+#brew install things.sh
+#brew install just # Just command runner
 # brew install testssl
-# brew install git-lfs
-# brew install lua
-# brew install lynx
-# brew install pigz
-# brew install pv
-# brew install rename
-# brew install tree
-# brew install vbindiff
-# brew install webkit2png
-# brew install zopfli
-brew install maclaunch # Check launch agents
+brew install git-lfs
+#brew install lua
+#brew install lynx
+#brew install pigz
+#brew install pv
+#brew install rename
+#brew install tree
+#brew install vbindiff
+#brew install webkit2png
+#brew install zopfli
+#brew install maclaunch # Check launch agents
 
 # Install and configure Tmux
-brew install tmux
+#brew install tmux
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+#git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Install TPM plugins.
 # TPM requires running tmux server, as soon as `tmux start-server` does not work
 # create dump __noop session in detached mode, and kill it when plugins are installed
-printf "Install TPM plugins\n"
-tmux new -d -s __noop >/dev/null 2>&1 || true
-tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "~/.tmux/plugins"
-"$HOME"/.tmux/plugins/tpm/bin/install_plugins || true
-tmux kill-session -t __noop >/dev/null 2>&1 || true
+# printf "Install TPM plugins\n"
+# tmux new -d -s __noop >/dev/null 2>&1 || true
+# tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "~/.tmux/plugins"
+# "$HOME"/.tmux/plugins/tpm/bin/install_plugins || true
+# tmux kill-session -t __noop >/dev/null 2>&1 || true
 
 # Remove outdated versions from the cellar.
 brew cleanup
